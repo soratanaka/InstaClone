dfab49fcfb44e3cd916eeaad0227aa4972857544
田中宙 <tanakachuu@tanakachuunoAir.elecom>
2022-10-13 11:25:40 +0900

    page route make

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,64 @@
 class PicturesController < ApplicationController
+  before_action :set_picture, only: %i[ show edit update destroy ]
+
+  def index
+    @pictures = Picture.all
+  end
+
+  def show
+  end
+
   def new
+    if params[:back]
+      @picture = Picture.new(picture_params)
+    else
+      @picture = Picture.new
+    end
+  end
+
+  def confirm
+    @picture = Picture.new(picture_params)
+  end
+
+  def edit
+  end
+
+  def create
+    @picture = Picture.new(picture_params)
+
+    respond_to do |format|
+      if @picture.save
+        format.html { redirect_to picture_url(@picture), notice: "Picture was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @picture.update(picture_params)
+        format.html { redirect_to picture_url(@picture), notice: "Picture was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @picture.destroy
+
+    respond_to do |format|
+      format.html { redirect_to pictures_url, notice: "Picture was successfully destroyed." }
+    end
+  end
+
+  private
+  def set_picture
+  @picture = Picture.find(params[:id])
+  end
+
+  def picture_params
+  params.require(:picture).permit(:image, :image_cache, :content).merge(user_id:current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  root 'sessions#new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
-  resources :picture
+  resources :pictures
 end
