2f53aeed383ca8cb2a757bb2dcbe8693ca69b27d
田中宙 <tanakachuu@tanakachuunoAir.elecom>
2022-10-13 16:20:30 +0900

    favorite make

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   root 'sessions#new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
-  resources :pictures
+  resources :pictures do
+    collection do
+      post :confirm
+    end
+  end
+  resources :favorites, only: [:create, :destroy]
 end
