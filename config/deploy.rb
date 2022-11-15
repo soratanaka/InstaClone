28c3cf7361dacd77c8f8e541eeed0e20417eaad3
田中宙 <tanakachuu@tanakachuunoAir.elecom>
2022-11-15 21:35:55 +0900

    deploy.rb refix

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,9 +30,6 @@ set :rbenv_type, :system
 # ただし挙動をしっかり確認したいのであれば :debug に設定する。
 set :log_level, :info
 
-# master.key用のシンボリックリンクを追加
-set :linked_files, %w{ config/master.key }
-
 namespace :deploy do
   desc 'Restart application'
   task :restart do
@@ -50,19 +47,6 @@ namespace :deploy do
     end
   end
 
-  desc 'upload master.key'
-  task :upload do
-    on roles(:app) do |host|
-      if test "[ ! -d #{shared_path}/config ]"
-        execute "mkdir -p #{shared_path}/config"
-      end
-        upload!('config/master.key', "#{shared_path}/config/master.key")
-      end
-    end
-    before :starting, 'deploy:upload'
-    after :finishing, 'deploy:cleanup'
-  end
-
   desc 'Run seed'
   task :seed do
     on roles(:app) do
@@ -74,15 +58,35 @@ namespace :deploy do
     end
   end
 
-  after 'deploy:publishing', 'deploy:restart'
-    namespace :deploy do
-    task :restart do
-    invoke 'unicorn:restart'
-  end
-
   after :publishing, :restart
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
     end
   end
+
+  # master.key用のシンボリックリンクを追加
+set :linked_files, %w{ config/master.key }
+
+# デプロイ処理が終わった後、Unicornを再起動するための記述
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+ task :restart do
+   invoke 'unicorn:restart'
+ end
+
+# 以下の記述により、GitHubに記録を残すことなく、master.keyを本番環境内に保存できる。
+ desc 'upload master.key'
+  task :upload do
+   on roles(:app) do |host|
+    if test "[ ! -d #{shared_path}/config ]"
+      execute "mkdir -p #{shared_path}/config"
+    end
+      upload!('config/master.key', "#{shared_path}/config/master.key")
+    end
+  end
+
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
+end
+end
